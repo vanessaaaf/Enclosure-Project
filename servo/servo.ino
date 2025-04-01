a99c472747aa9c9e42aefb281fbf96e5b9d695f9
vanessaaaf <floresvanessa14@icloud.com>
2025-04-01 13:28:49 -0500

    Add files via upload

---
+++ servo/servo.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo myservo;
+const int buttonPin = 2;
+
+
+void setup() {
+myservo.attach(9);
+pinMode(buttonPin, INPUT);
+
+}
+
+void loop() {
+ if(digitalRead(buttonPin) == HIGH) {
+  myservo.write(360);
+ }else {
+  myservo.write(0);
+ }
+}
